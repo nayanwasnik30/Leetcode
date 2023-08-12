c9fe4ab3347f77773f0d397455320d8abd3bf8c0
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2023-08-12 23:13:59 +0530

    Time: 2 ms (78.09%), Space: 43.8 MB (82.98%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+   public List<List<Integer>> combinationSum(int[] a, int t) {
+        List<List<Integer>> ans = new ArrayList<>(); // ans list
+        List<Integer> ls = new ArrayList<>();  // our inside list to store different combinations
+        int index = 0; // starting from index 0
+        combSum(index,ans,a,t,ls); // recursive function
+        return ans;
+    }
+    static void combSum(int i,List<List<Integer>>ans,int a[],int t,List<Integer> ds) {
+        // Base condition
+		if(i == a.length) {
+            if(t == 0) {
+                ans.add(new ArrayList<>(ds)); // ArrayList constructor constructs a list of specified collection . in our case it is List<Integer> ds
+            }
+            return;
+        }
+		
+		// Check if the element at index is smaller than the target or not
+        if(a[i] <= t) {
+            ds.add(a[i]);
+            combSum(i,ans,a,t-a[i],ds); // calls for same index and decreasing target by a[i]
+            ds.remove(ds.size()-1); // during backtracking we will remove the added element in ds
+        }
+        combSum(i+1,ans,a,t,ds); // call for next index
+    }
+}
