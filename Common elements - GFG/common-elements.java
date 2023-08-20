ea259c03c927781365eb32101ccd759cd983c9f9
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2023-08-20 22:51:35 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here 
+        HashMap <Integer,Integer> map1 = new HashMap<>();
+        HashMap <Integer,Integer> map2 = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int ele :A){
+                map1.put(ele,1);
+        }
+        
+          for(int ele :B){
+            if(map1.containsKey(ele)){
+                map2.put(ele,1);
+            }
+        }
+          for(int ele :C){
+            if(map2.containsKey(ele)){
+                list.add(ele);
+                map2.remove(ele);
+            }
+        }
+        Collections.sort(list);   
+        return list;
+        
+    }
+}
