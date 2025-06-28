12138a5e3848a8af1495b7b30a342b5d8b4e910e
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-06-28 07:53:49 +0530

    Time: 1 ms (93.16%), Space: 44.9 MB (18.89%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> ans;
+    public List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
+        boolean [] bool = new boolean[nums.length];
+        helper(nums,0,bool,list);
+        return ans;
+    }
+    public void helper(int [] arr,int idx,boolean [] bool,List<Integer> list){
+        if(arr.length==list.size()){
+            ans.add(new ArrayList<>(list));
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(bool[i]==true) continue;
+            list.add(arr[i]);
+            bool[i]=true;
+            helper(arr,idx,bool,list);
+            bool[i]=false;
+            list.remove(list.size()-1);
+        }
+
+    }
+}
