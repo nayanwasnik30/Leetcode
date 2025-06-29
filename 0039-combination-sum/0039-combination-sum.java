e9732b23aad288a2796f2252f7f05a8113edc29a
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-06-29 10:50:53 +0530

    Time: 4 ms (12.01%), Space: 44.7 MB (64.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,27 @@
 class Solution {
-   public List<List<Integer>> combinationSum(int[] a, int t) {
-        List<List<Integer>> ans = new ArrayList<>(); // ans list
-        List<Integer> ls = new ArrayList<>();  // our inside list to store different combinations
-        int index = 0; // starting from index 0
-        combSum(index,ans,a,t,ls); // recursive function
+    List<List<Integer>> ans;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        ans= new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        helper(candidates,target,0,list);
         return ans;
     }
-    static void combSum(int i,List<List<Integer>>ans,int a[],int t,List<Integer> ds) {
-        // Base condition
-		if(i == a.length) {
-            if(t == 0) {
-                ans.add(new ArrayList<>(ds)); // ArrayList constructor constructs a list of specified collection . in our case it is List<Integer> ds
+    public void helper(int []arr,int target,int idx,List<Integer> list){
+        if(target<0){
+            return ;
+        }
+        if(idx==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(list));
             }
             return;
         }
-		
-		// Check if the element at index is smaller than the target or not
-        if(a[i] <= t) {
-            ds.add(a[i]);
-            combSum(i,ans,a,t-a[i],ds); // calls for same index and decreasing target by a[i]
-            ds.remove(ds.size()-1); // during backtracking we will remove the added element in ds
-        }
-        combSum(i+1,ans,a,t,ds); // call for next index
+
+        list.add(arr[idx]);
+        helper(arr,target-arr[idx],idx,list);
+        list.remove(list.size()-1);
+
+        helper(arr,target,idx+1,list);
     }
 }
