2eb9a47d72544ddeaae243c74303c0e8f5dbdce9
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-07-23 18:50:30 +0530

    Time: 50 ms (10.33%), Space: 66.2 MB (59.65%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        HashMap<Integer ,Boolean > map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],true);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i]-1)==true){
+                map.put(nums[i],false);
+            }
+        }
+        int max=0;
+        for(int value:map.keySet()){
+            if(map.get(value)==true){
+                int curr=1;
+                while(map.containsKey(value + 1)!=false){
+                    curr++;
+                    value++;
+                }
+                max=Math.max(max,curr);
+            }
+           
+        }
+
+
+        return max;
+
+    }
+}
