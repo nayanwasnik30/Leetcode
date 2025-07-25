0e56ce83483d62a16499ea82d78f4b3aaa40e375
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-07-26 01:13:11 +0530

    Time: 0 ms (100%), Space: 45.3 MB (71.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
+
         for(int i=0;i<n;i++){
-            if(nums[i]%(n+1)<n){
-                int value=nums[i]%(n+1);
-                nums[value]=nums[value]+(n+1);
+             int val=nums[i]%(n+1);
+            if(val<n){
+                nums[val]=nums[val]+(n+1);
             }
         }
         for(int i=0;i<n;i++){
-            if(nums[i]<(n+1)) return i;
+            if(nums[i]<(n+1)){
+                return i;
+            }
         }
         return n;
     }
