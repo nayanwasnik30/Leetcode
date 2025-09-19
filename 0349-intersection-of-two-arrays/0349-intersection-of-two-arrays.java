ac22a93bf27b24fc54358134de15c235232cea81
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-09-20 02:11:20 +0530

    Time: 3 ms (49.89%), Space: 43.4 MB (63.77%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> freq= new HashMap<>();
-
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
-            freq.put(nums1[i],freq.getOrDefault(nums1[i],0)+1);
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
         }
-        ArrayList<Integer> inter = new ArrayList<>();
-
+        ArrayList<Integer> arr= new ArrayList<>();
         for(int i=0;i<nums2.length;i++){
-            if(freq.containsKey(nums2[i])==true){
-                inter.add(nums2[i]);
-                freq.remove(nums2[i]);
+            if(map.containsKey(nums2[i])==true){
+                arr.add(nums2[i]);
+                map.remove(nums2[i]);
             }
         }
-        int [] arr= new int [inter.size()];
-        for(int i=0;i<inter.size();i++){
-            arr[i]=inter.get(i);
+        int arr1 []= new int [arr.size()];
+        for(int i=0;i<arr.size();i++){
+           arr1[i]=arr.get(i);
         }
-        return arr;
+        return arr1;
     }
-
 }
