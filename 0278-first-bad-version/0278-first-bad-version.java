f405b5ee4c45d2a10c9a63ca3824a8060fa47bf5
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-09-28 16:47:39 +0530

    Time: 24 ms (97.81%), Space: 40.7 MB (28.39%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,13 +7,12 @@ public class Solution extends VersionControl {
         int right=n;
         while(left<=right){
             int mid=left+(right-left)/2;
-           if(isBadVersion(mid)==false){
-                left=mid+1; 
+            if(isBadVersion(mid)==false){
+                left=mid+1;
             }else{
                 right=mid-1;
             }
         }
-
         return left;
     }
 }
