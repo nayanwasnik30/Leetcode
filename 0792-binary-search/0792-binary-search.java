8591caed23cda056e980eb1acdd77dbb7bf2191a
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-09-28 16:18:24 +0530

    Time: 0 ms (100%), Space: 46 MB (23.94%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,19 +1,20 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int left =0;
-        int right=arr.length-1;
-
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(arr[mid]==target){
-                return mid;
-            }else if(arr[mid]<target){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
+    public int search(int[] nums, int target) {
+       int n= nums.length;
+       int left=0;
+       int right=n-1;
+       while(left<=right){
+        int mid=(left+right)/2;
+        if(nums[mid]==target){
+            return mid;
         }
+        else if(nums[mid]<target){
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+       }
 
-        return -1;
+       return -1;
     }
 }
