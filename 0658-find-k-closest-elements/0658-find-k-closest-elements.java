aef731a3fcd50b5d9afbca895ad0f13775c78cf9
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-01 13:47:18 +0530

    Time: 4 ms (95.88%), Space: 45.7 MB (86.57%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,10 +1,29 @@
 class Solution {
-    public int floorVal(int []arr,int target){
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int floors=floor(arr,x);
+        int ceil=floors+1;
+        for(int count=0;count<k;count++){
+            int a=(floors==-1) ? Integer.MAX_VALUE:(x-arr[floors]);
+            int b=(ceil==arr.length)?Integer.MAX_VALUE:(arr[ceil]-x);
+            if(a<=b){
+                floors--;
+            }else{
+                ceil++;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=floors+1;i<ceil;i++){
+            list.add(arr[i]);
+        }
+        return list;
+    }
+    public int floor(int []arr,int target){
         int left=0;
         int right=arr.length-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
 
+        while(left<=right){
+            int mid =left+(right-left)/2;
             if(arr[mid]==target){
                 right=mid-1;
             }else if(arr[mid]<target){
@@ -15,26 +34,5 @@ class Solution {
         }
 
         return right;
-    }
-    public List<Integer> findClosestElements(int[] arr, int k, int target) {
-        List <Integer> list= new ArrayList<>();
-        int floor=floorVal(arr,target);
-        int ceil=floor+1;
-
-        for(int i=0;i<k;i++){
-            int fval=(floor==-1) ? Integer.MAX_VALUE : (target-arr[floor]);
-            int cval=(ceil==arr.length) ? Integer.MAX_VALUE : (arr[ceil]-target)  ;
-
-            if(fval<=cval){
-                floor--;
-            }else{
-                ceil++;
-            }
-        }
-
-        for(int i=floor+1;i<ceil;i++){
-            list.add(arr[i]);
-        }
-        return list;
     }
 }
