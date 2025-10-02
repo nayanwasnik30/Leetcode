30603a5f89b8f5478627cab927712511478a0b77
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-03 00:08:38 +0530

    Time: 17 ms (57.7%), Space: 47.7 MB (6%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,37 +1,36 @@
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
         Arrays.sort(heaters);
-        int mradius=0;
-        for(int ho:houses){
-            int radius=binary(ho,heaters);
-            mradius=Math.max(mradius,radius);
+        int minradius=0;
+        for(int i=0;i<houses.length;i++){
+            int radius =search(houses[i],heaters);
+            minradius=Math.max(minradius,radius);
         }
-        return mradius;
+        return minradius;
     }
-    public int binary(int house,int [] heaters){
-        int floor=floorVal(house,heaters);
+    public int search(int house,int [] heaters){
+        int floor=floorarray(house,heaters);
         int ceil=floor+1;
 
-        int fdis=(floor==-1) ?Integer.MAX_VALUE:(house-heaters[floor]);
-        int cdis=(ceil==heaters.length) ?Integer.MAX_VALUE:(heaters[ceil]-house);
-        return Math.min(fdis,cdis);
+        int dis1=(floor==-1) ? Integer.MAX_VALUE:(house-heaters[floor]);
+        int dis2=(ceil==heaters.length)?Integer.MAX_VALUE:(heaters[ceil]-house);
+        return Math.min(dis1,dis2);
     }
-    public int floorVal(int target,int []arr){
+    public int floorarray(int target,int []heaters){
         int left=0;
-        int right=arr.length-1;
+        int right=heaters.length-1;
 
         while(left<=right){
             int mid=left+(right-left)/2;
-            
-            if(arr[mid]==target){
+            if(heaters[mid]==target){
                 right=mid-1;
-            }else if(arr[mid]<target){
+            }
+            else if(heaters[mid]<target){
                 left=mid+1;
             }else{
                 right=mid-1;
             }
         }
-
         return right;
     }
 }
