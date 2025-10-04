3e768288893ec70e47f7bab2b045e4f4f0da9acd
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-05 01:04:26 +0530

    Time: 0 ms (100%), Space: 56.2 MB (72.91%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,13 +1,15 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int left=1;int right=arr.length-2;
+        int left=1;
+        int right=arr.length-2;
 
         while(left<=right){
             int mid=left+(right-left)/2;
-            
-            if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid]){
+
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
                 return mid;
-            }else if(arr[mid]>arr[mid-1]){
+            }
+            else if(arr[mid]>arr[mid-1]){
                 left=mid+1;
             }else{
                 right=mid-1;
@@ -15,6 +17,5 @@ class Solution {
         }
 
         return -1;
-        
     }
 }
