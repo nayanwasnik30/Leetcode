cd64806c53c23010351a2f95712d424b2fee6ecd
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-05 02:08:16 +0530

    Time: 1 ms (84.79%), Space: 41.3 MB (21.2%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -5,15 +5,13 @@ class Solution {
 
         while(left<=right){
             long mid=left+(right-left)/2;
-
-            if(mid*mid==x){
-                return (int)mid;
-            }else if(mid*mid<x){
+            if(mid*mid<=x){
                 left=mid+1;
             }else{
                 right=mid-1;
             }
         }
+
         return (int)right;
     }
 }
