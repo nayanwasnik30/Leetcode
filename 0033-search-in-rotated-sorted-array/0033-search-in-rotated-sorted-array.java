77cc49d803868766e0706f660c215611a8e22002
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-04 17:30:16 +0530

    Time: 0 ms (100%), Space: 42.5 MB (20.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,42 +1,44 @@
 class Solution {
-    public int pindex(int []arr){
-        int left=0;
-        int right=arr.length-1;
-        while(left<=right){
-            int mid =left+(right-left)/2;
-
-            if(arr[0]<=arr[mid]){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
-        }
-        return left;
-    }
     public int search(int[] nums, int target) {
-        int piviot=pindex(nums);
-        if(piviot==0){
-            return binary(nums,0,nums.length-1,target);
+        int n=nums.length-1;
+        int pviot=pviotfind(nums,target);
+        if(pviot==0){
+            return sorted(nums,target,0,nums.length-1);
         }
         else if(target<nums[0]){
-            return binary(nums,piviot,nums.length-1,target);
+            return sorted(nums,target,pviot,nums.length-1);
         }else{
-            return binary(nums,0,piviot-1,target);
+            return sorted(nums,target,0,pviot-1);
         }
-    }
-    public int binary(int []arr,int left,int right,int target){
-        
-        while(left<=right){
-            int mid=left+(right-left)/2;
 
-            if(arr[mid]==target){
+    }
+    public int sorted(int []nums,int target,int left,int right){
+         while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
                 return mid;
-            }else if(arr[mid]<target){
+            }
+            if(nums[mid]<target){
                 left=mid+1;
             }else{
                 right=mid-1;
             }
         }
+
         return -1;
+    }public int pviotfind(int []nums,int target){
+        int left=0;
+        int right=nums.length-1;
+        
+         while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=nums[0]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+
+        return left;
     }
 }
