859b53cf59b893b50b33f0d0e1c58f3fd663b4b2
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-06 01:48:53 +0530

    Time: 5 ms (99.39%), Space: 46.1 MB (40.69%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,6 +1,19 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-         for (int i = 0; i < matrix.length; i++) if (Arrays.binarySearch(matrix[i], target) > -1) return true;
+    public boolean searchMatrix(int[][] mat, int target) {
+        //stair case problem;
+        int n=mat[0].length;
+        int m=mat.length;
+        int left=0; //left pointer travel up to down(row++);
+        int right=n-1; //right pointer travel right to left (col--)
+        while(left<m && right>-1){
+            if(mat[left][right]==target){
+                return true;
+            }else if(mat[left][right]<target){
+               left++;
+            }else{
+                right--;
+            }
+        }
         return false;
     }
 }
