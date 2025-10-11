823911029891284119e1e8d0937d6bdf1cb0ef9a
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-11 21:48:21 +0530

    Time: 0 ms (100%), Space: 42.5 MB (28.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean searchMatrix(int[][] mat, int target) {
-        //stair case problem;
+        //first implement Binary serch
+        //fatten the 3d to 2d ;
         int n=mat[0].length;
         int m=mat.length;
         int left=0;
-        int right=(n*m)-1;
+        int right=(n*m)-1;//total length
         while(left<=right){
             int mid=left+(right-left)/2;
-            int l=mid/n; int r=mid%n;
-            if(mat[l][r]==target){
+            //left and right athe pointer where to but do not know whts in there 
+            //there fore convert pointer to get cordinates from mid;
+            int l=mid/n;int r=mid%n;
+            int sum=mat[l][r];
+            if(sum==target){
                 return true;
-            }else if(mat[l][r]<target){
+            }else if(sum<target){
                 left=mid+1;
             }else{
                 right=mid-1;
             }
         }
+
         return false;
     }
 }
