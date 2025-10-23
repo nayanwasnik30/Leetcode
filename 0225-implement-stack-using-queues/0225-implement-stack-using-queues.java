57d046c8f0bc593f019aab6b2b66375d67dbdcb5
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-23 17:32:03 +0530

    Time: 0 ms (100%), Space: 40.9 MB (99.4%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,39 +1,30 @@
 class MyStack {
-    Queue<Integer> q1;
-    // Queue<Integer> q2;
-
-    public MyStack() {
-        q1 = new LinkedList<>();
-        // q2 = new LinkedList<>();
-    }
+    Queue<Integer> stack= new ArrayDeque<>();
     
     public void push(int x) {
-        // q2.add(x);
-        // while(!q1.isEmpty()){
-        //     q2.add(q1.remove());
-        // }
-        // while(!q2.isEmpty()){
-        //     q1.add(q2.remove());
-        // }
-        q1.add(x);
-        for(int i=0;i<q1.size()-1;i++){
-            q1.add(q1.remove());
-        }
+        stack.add(x);
     }
     
     public int pop() {
-        if(q1.isEmpty()){
-            return -1;
+        for(int idx=1;idx<stack.size();idx++){
+            stack.add(stack.remove());
         }
-        return  q1.remove();
+        int ans=stack.remove();
+        return ans;
     }
     
     public int top() {
-        return q1.peek();
+        for(int idx=1;idx<stack.size();idx++){
+            stack.add(stack.remove());
+        }
+        int ans=stack.remove();
+        stack.add(ans);
+        return ans;
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        if(stack.size()==0) return true;
+        else return false;
     }
 }
 
