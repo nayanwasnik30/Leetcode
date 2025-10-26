11c32f43a0e31ead766eb064f1f6810cbbb95b8c
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-26 22:54:58 +0530

    Time: 18 ms (63.79%), Space: 43.9 MB (78.27%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,21 +7,21 @@ class Solution {
             return list;
         }
         Arrays.sort(nums);
-        for(int i=n-1;i>=3;i--){
-            if (i < n - 1 && nums[i] == nums[i + 1]) continue;
-            for(int j=i-1;j>=2;j--){
-                if (j < i - 1 && nums[j] == nums[j + 1]) continue;
-                int left=0;
-                int right=j-1;
+        for(int i=0;i<nums.length-3;i++){
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<nums.length-2;j++){
+                if (j>i+1&& nums[j]==nums[j-1]) continue;
+                int left=j+1;
+                int right=nums.length-1;
                 while(left<right){
                     long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
                     if(sum==target){
                         list.add(Arrays.asList(nums[left],nums[right],nums[i],nums[j]));
                         // now to skip same element ;
-                        while(nums[left]==nums[left+1] && left<right){
+                        while(left<right && nums[left]==nums[left+1]){
                             left++;
                         }
-                        while(nums[right]==nums[right-1] && left<right){
+                        while(right>left && nums[right]==nums[right-1]){
                             right--;
                         }
                         left++;
