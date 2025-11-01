cd77c48a37aef8b4c3e7d86f6fbe98d7e6ee491e
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-01 21:25:36 +0530

    Time: 5 ms (84.44%), Space: 47.1 MB (6.14%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[] asteroidCollision(int[] ast) {
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<ast.length;i++){
-            int temp=ast[i];
-            while(stack.size()>0 && stack.peek()>0 && temp < 0){//check when  is the collsion(+ vs -)
-                int sum=stack.peek()+temp;
-                if(sum<0){  //if sum is < 0
-                    stack.pop();
-                }
-                else if(sum>0){ //if sum is > 0 
-                    temp=0;
-                    break;
-                }else { //if sum is ==0
-                    temp=0;
-                    stack.pop();
-                    break;
-                }
+       Stack<Integer> stack = new Stack<>();
+       int idx=0;
+       while(idx<ast.length){
+            int right=ast[idx];
+            // collision (+ve vs -ve)
+            // who will win ?
+            // whos value is greater 
+            if(right>0) stack.push(right);
+            else {
+            if(stack.size()==0 || stack.peek()<0){
+                stack.push(right);
             }
-            if(temp!=0)stack.push(temp);
-        }
-        int n  = stack.size();
-        int arr[] = new int[n];
-        while(n > 0) {
-             n--;
-            arr[n] = stack.pop();
-           
-        }
-        return arr;
+            else if(stack.size()>0 && stack.peek() < -right){
+                stack.pop();
+                idx--;
+            }else if(stack.size()>0 && stack.peek()==-right){
+                stack.pop();
+            }
+            }
+            idx++;
+       } 
+
+       int [] arr= new int[stack.size()];
+       int n=stack.size();
+       while(n>0){
+        n--;
+        arr[n]=stack.pop();
+       }
+       return arr;
     }
 }
