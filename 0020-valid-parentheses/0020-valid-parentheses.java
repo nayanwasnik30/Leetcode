92b3408b33461d3d2bdcba726f74b811e0f28d00
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-04 23:16:44 +0530

    Time: 3 ms (60.17%), Space: 43.2 MB (6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        if(s.length()<=1) return false;
+        Stack<Character> stack= new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='['||ch=='{'){
+            if(ch=='('|| ch=='[' || ch=='{'){
                 stack.push(ch);
-            }
-            else{
+            }else{
                 if(stack.size()==0) return false;
-                if(ch==')'&& stack.peek()=='('){
+                char open=stack.peek();
+                if(open=='(' && ch==')'){
                     stack.pop();
-                }
-                else if(ch==']' && stack.peek()=='['){
+                }else if(open=='[' && ch==']'){
                     stack.pop();
-                }
-                else if(ch=='}' && stack.peek()=='{'){
+                }else if(open=='{' && ch=='}'){
                     stack.pop();
+                }else{
+                    return false;
                 }
-                else return false;
             }
         }
-
-
         if(stack.size()==0) return true;
-        else return false;
+        else{
+        return false;
+        }
     }
 }
