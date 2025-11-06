9174cfa13b589e469ebad7a6b680f40d521befd4
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-06 23:54:47 +0530

    Time: 3 ms (97.12%), Space: 52 MB (98.47%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -4,7 +4,7 @@ class Solution {
         for(int right=0;right<nums.length;right++){
             if(nums[right]==0) zero++;
             while(zero>k){
-                if(nums[left]==0)zero--;
+               if(nums[left]==0) zero--;
                 left++;
             }
             max=Math.max(max,right-left+1);
