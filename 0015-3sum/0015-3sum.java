f33f60db09951026054d815ade2277ced923684d
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-08 17:01:19 +0530

    Time: 32 ms (41.54%), Space: 59.1 MB (5.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans= new ArrayList<>();
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length-1;i++){
             if(i>0 && nums[i-1]==nums[i]){
                 continue;
             }
