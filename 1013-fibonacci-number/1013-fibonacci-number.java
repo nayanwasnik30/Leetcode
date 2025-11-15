75739bc4b94c3b2c148fbfc550dbf423267954fc
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-15 23:55:32 +0530

    Time: 10 ms (11.88%), Space: 41.9 MB (17.31%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,8 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        } //base case 
-        if(n==1){
-            return 1;
-        }
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-        int ans= fib(n-1)+fib(n-2);
-        return ans;
+        return fib(n-1)+fib(n-2);
     }
 }
