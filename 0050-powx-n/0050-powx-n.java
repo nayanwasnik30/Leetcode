0f1f686f79ab56ac2638c43d7be512892ae88b22
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-16 13:45:22 +0530

    Time: 0 ms (100%), Space: 48.4 MB (5.81%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n<0){
-            return 1.0/helper(x,-1l*n);
-        }
+        if(n<0) return 1.0/helper(x,-1L*n);
         return helper(x,n);
     }
-    public double helper(double x,long n){
-         if(n==0){
-            return 1.0;
-        }
-        if(n%2 != 0){   
-        double ans=x*helper(x,n-1);
-        return ans;
-        }
-        
-        double ans=helper(x,n/2);
-        ans=ans*ans;
-        return ans;
+     public double helper(double x, long n) {
+       if(n==0) return 1.0;
+
+       if(n%2 != 0){
+        double sans=helper(x,n-1);
+        return sans*x;
+       }
+       double sans=helper(x,n/2);
+       return sans*sans;
     }
 }
