f8dac778ba78631af41a71114734144b45c6b9d8
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-18 00:17:07 +0530

    Time: 20 ms (9.35%), Space: 47.6 MB (5.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        s = s.replaceAll(" ", "");
+        s = s.replaceAll(":", "");
+        s = s.replaceAll(",", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
-
-        int i = 0;
-        int j = s.length() - 1;
-
-        while (i < j) {
-            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
-                i++;
-            }
-            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
-                j--;
-            }
-
-            if (s.charAt(i) != s.charAt(j)) {
+         //System.out.println(s);
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
-
             i++;
             j--;
         }
